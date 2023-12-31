431adb97411fdcac203a1b1f4257cc2324befd86
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-30 21:32:01 -0700

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strArray = Array.from(str);
+    return strArray.reverse().join('');
 };
-
 // Do not edit below this line
 module.exports = reverseString;
