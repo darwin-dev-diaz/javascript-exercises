033dd7b28572b5a3463374479c4c182c255283d6
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-30 21:26:51 -0700

    Complete first and second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, repetitions) {
+    let strMultiplied = ''
+    if(repetitions < 0) return 'ERROR';
+    for(let i = 0; i < repetitions; i++){
+        strMultiplied = strMultiplied + str
+    }
+    return strMultiplied;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
