2adb04a2eda0599060f0a89ed21c871533b174a6
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-31 20:34:15 -0700

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elementsToRemove) {
+    for(const element of elementsToRemove){
+        if(arr.includes(element)){
+            arr.splice(arr.indexOf(element),1);
+        }
+    }
+    return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
