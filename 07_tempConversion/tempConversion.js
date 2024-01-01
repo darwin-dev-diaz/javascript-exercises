12060c8b2f7cf2a2ae2f4c0f1f11cfcd7b6ebfc6
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-31 21:01:33 -0700

    Complete seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  let raw = (degreesFahrenheit-32)*5/9;
+  return raw % 1 === 0 ? raw : Number(raw.toFixed(1));
 };
-
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let raw = (degreesCelsius*9/5)+32;
+  return raw % 1 === 0 ? raw : Number(raw.toFixed(1));
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
