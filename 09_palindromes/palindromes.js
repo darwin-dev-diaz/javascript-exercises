255d5ce4a155cc0e914eebe3983ae7a98f3f47c3
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-01-29 08:45:31 -0700

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (stringy) {
+    const cleanedArr = stringy.match(/\w/g,' ').map((char=>char.toLowerCase()));
 
+    return JSON.stringify(cleanedArr) === JSON.stringify(cleanedArr.reverse()) ? true : false;
 };
 
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;
