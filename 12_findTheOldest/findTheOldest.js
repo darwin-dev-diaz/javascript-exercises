341a7c55cc3b5ebadb0863667a1685aa07129924
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-01-30 08:52:28 -0700

    Finish findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrOfPeople) {
+    const testArr = arrOfPeople.sort((a,b)=>{
+        const aAge = a.yearOfDeath ? a.yearOfBirth - a.yearOfDeath : a.yearOfBirth - 2023;
+        const bAge = b.yearOfDeath ? b.yearOfBirth - b.yearOfDeath : b.yearOfBirth - 2023;
 
+        if(aAge > bAge) return 1;
+        return -1;
+    });
+
+    return testArr[0];
 };
+    
+    const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+console.log(findTheOldest(people));
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
