e4a1afea7df9e2c5a5cf3f677a3621b4afebb524
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-01-30 08:42:10 -0700

    Finish fib exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
 
+    if(Math.sign(num) === -1) return "OOPS";
+    else if(num === 0) return 0;
+    else if(num === 1 || num === 2) return 1;
+    return (fibonacci(num-1) + fibonacci(num-2));
 };
+
+console.log(fibonacci(6));
+console.log(typeof +'1');
 
 // Do not edit below this line
 module.exports = fibonacci;
